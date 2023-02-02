974f8ae6ce3abcd160c9867a247c4d34c2e0e596
Rita Shander <iamshizooo@gmail.com>
2023-02-02 12:19:22 +0300

    work2 done q q! clear

---
+++ Homework4/work2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int num = 25;
+System.Console.WriteLine(GetSumNumbers(num));
+
+static int GetSumNumbers(int num)
+{
+    int s = 0;
+    for(int i = 0; i <= num; i++)
+    {
+        s = s + (num % 10);
+        num = num / 10;
+    }
+    return s;
+}
