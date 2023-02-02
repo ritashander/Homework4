aef7fd13059bc8580f49d059a59e72c37ff98474
Rita Shander <iamshizooo@gmail.com>
2023-02-02 11:59:46 +0300

    work1 done

---
+++ Homework4/work1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (А и В) и возводит число А в натуральную степень В.
+
+
+int numberA = 2;
+int numberB = 3;
+System.Console.WriteLine(numberA);
+System.Console.WriteLine(numberB);
+
+System.Console.WriteLine(Exponentiation(numberA, numberB));
+
+int Exponentiation(int numberA, int numberB)
+{
+    int result = 1;
+    for(int i=1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+    return result;
+}
