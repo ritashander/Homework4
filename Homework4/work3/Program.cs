18de2f45d96f97adc6840186648018d2655b8a76
Rita Shander <iamshizooo@gmail.com>
2023-02-02 12:25:17 +0300

    work 3 done

---
+++ Homework4/work3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] arr = GetRandomArray(8);
+PrintArray(arr);
+
+int[] GetRandomArray(int length)
+{
+    int[] result = new int[length];
+
+    for(int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(0, 10);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach(int item in arr)
+    {
+        System.Console.Write(item+ ",");
+    }
+}
